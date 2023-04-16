591a81f01248869bc9f3897f66f5f49783e59fa5
Holden <120606057+HCN1202@users.noreply.github.com>
2023-04-16 00:24:55 +0000

    codespace attempt

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
 const pc = player
-
-const npc = enemy
-
-moveBy(newImage('assets/New Piskel.png')).to(200, 200)
--- player.js
+++ player.js
@@ -2,7 +2,7 @@ function palyer(x,y){
     const element = newImage('img/New Piskel.png)')
     element.style.zIndex = 1;
 
-walk(element).arrowKeys(x, y)
+walk(element).moveWithArrowKeys(x,y)
 return {
     element: element
 }
