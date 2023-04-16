8bab409f72028f995b8031662a641a96aac04496
Holden <120606057+HCN1202@users.noreply.github.com>
2023-04-15 20:04:36 -0400

    Add files via upload
    The files would only update on local computer so I directly uploaded them

---
+++ index.js
@@ -0,0 +1,5 @@
+const pc = player
+
+const npc = enemy
+
+moveBy(newImage('assets/New Piskel.png')).to(200, 200)
---
+++ player.js
@@ -0,0 +1,10 @@
+function palyer(x,y){
+    const element = newImage('img/New Piskel.png)')
+    element.style.zIndex = 1;
+
+walk(element).arrowKeys(x, y)
+return {
+    element: element
+}
+
+}
